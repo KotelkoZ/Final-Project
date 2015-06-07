dbd4484d9d50e0a1a7a809f65e7291a47c2d51fd
KotelkoZ <KotelkoZ@gmail.com>
2015-06-07 12:29:27 -0600

    Add login

---
+++ Farm Tracker/Farm Tracker/Login_Form.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Farm_Tracker
+{
+    partial class Login_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.password_TextBox = new System.Windows.Forms.TextBox();
+            this.operator_ID_Textbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // password_TextBox
+            // 
+            this.password_TextBox.Location = new System.Drawing.Point(200, 146);
+            this.password_TextBox.Name = "password_TextBox";
+            this.password_TextBox.Size = new System.Drawing.Size(190, 31);
+            this.password_TextBox.TabIndex = 7;
+            // 
+            // operator_ID_Textbox
+            // 
+            this.operator_ID_Textbox.Location = new System.Drawing.Point(200, 98);
+            this.operator_ID_Textbox.Name = "operator_ID_Textbox";
+            this.operator_ID_Textbox.Size = new System.Drawing.Size(190, 31);
+            this.operator_ID_Textbox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Operator ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 303);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.password_TextBox);
+            this.Controls.Add(this.operator_ID_Textbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox password_TextBox;
+        private System.Windows.Forms.TextBox operator_ID_Textbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
