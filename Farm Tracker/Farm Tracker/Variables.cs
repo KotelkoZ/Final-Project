08335f046cf6e23c516a5bd6194fa41b2686c67f
KotelkoZ <KotelkoZ@gmail.com>
2015-06-09 21:48:20 -0600

    Add connection string

---
+++ Farm Tracker/Farm Tracker/Variables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm_Tracker
+{
+    class Variables
+    {
+        public static string CONNECTIONSTRING = "Data Source=ZAKS-XPS15/FARMTRACKER;Initial Catalog=Farm_Tracker;Integrated Security=True";
+    }
+}
