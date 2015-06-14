7f33f0af17550e95f7151ba45cd5e35c47aef24f
KotelkoZ <KotelkoZ@gmail.com>
2015-06-14 11:09:47 -0600

    Add operator form/color scheme data

---
+++ Farm Tracker/Farm Tracker/Operator_Form.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Farm_Tracker
+{
+    partial class Operator_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.operator_ID_Label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.first_Name_Label = new System.Windows.Forms.Label();
+            this.last_Name_Label = new System.Windows.Forms.Label();
+            this.language_Label = new System.Windows.Forms.Label();
+            this.first_Name_TextBox = new System.Windows.Forms.TextBox();
+            this.last_Name_TextBox = new System.Windows.Forms.TextBox();
+            this.language_TextBox = new System.Windows.Forms.TextBox();
+            this.update_Password_Button = new System.Windows.Forms.Button();
+            this.update_Operator_Button = new System.Windows.Forms.Button();
+            this.save_Button = new System.Windows.Forms.Button();
+            this.new_Operator_Button = new System.Windows.Forms.Button();
+            this.password_Label = new System.Windows.Forms.Label();
+            this.password_TextBox = new System.Windows.Forms.TextBox();
+            this.confirm_Password_Label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(12, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(343, 479);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(425, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Operator ID";
+            // 
+            // operator_ID_Label
+            // 
+            this.operator_ID_Label.AutoSize = true;
+            this.operator_ID_Label.Location = new System.Drawing.Point(548, 68);
+            this.operator_ID_Label.Name = "operator_ID_Label";
+            this.operator_ID_Label.Size = new System.Drawing.Size(122, 25);
+            this.operator_ID_Label.TabIndex = 3;
+            this.operator_ID_Label.Text = "Operator ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(431, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(432, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(439, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Language";
+            // 
+            // first_Name_Label
+            // 
+            this.first_Name_Label.AutoSize = true;
+            this.first_Name_Label.Location = new System.Drawing.Point(548, 118);
+            this.first_Name_Label.Name = "first_Name_Label";
+            this.first_Name_Label.Size = new System.Drawing.Size(116, 25);
+            this.first_Name_Label.TabIndex = 7;
+            this.first_Name_Label.Text = "First Name";
+            // 
+            // last_Name_Label
+            // 
+            this.last_Name_Label.AutoSize = true;
+            this.last_Name_Label.Location = new System.Drawing.Point(548, 172);
+            this.last_Name_Label.Name = "last_Name_Label";
+            this.last_Name_Label.Size = new System.Drawing.Size(115, 25);
+            this.last_Name_Label.TabIndex = 8;
+            this.last_Name_Label.Text = "Last Name";
+            // 
+            // language_Label
+            // 
+            this.language_Label.AutoSize = true;
+            this.language_Label.Location = new System.Drawing.Point(548, 225);
+            this.language_Label.Name = "language_Label";
+            this.language_Label.Size = new System.Drawing.Size(108, 25);
+            this.language_Label.TabIndex = 9;
+            this.language_Label.Text = "Language";
+            // 
+            // first_Name_TextBox
+            // 
+            this.first_Name_TextBox.Location = new System.Drawing.Point(553, 111);
+            this.first_Name_TextBox.Name = "first_Name_TextBox";
+            this.first_Name_TextBox.Size = new System.Drawing.Size(207, 31);
+            this.first_Name_TextBox.TabIndex = 10;
+            // 
+            // last_Name_TextBox
+            // 
+            this.last_Name_TextBox.Location = new System.Drawing.Point(553, 166);
+            this.last_Name_TextBox.Name = "last_Name_TextBox";
+            this.last_Name_TextBox.Size = new System.Drawing.Size(207, 31);
+            this.last_Name_TextBox.TabIndex = 11;
+            // 
+            // language_TextBox
+            // 
+            this.language_TextBox.Location = new System.Drawing.Point(553, 222);
+            this.language_TextBox.Name = "language_TextBox";
+            this.language_TextBox.Size = new System.Drawing.Size(207, 31);
+            this.language_TextBox.TabIndex = 12;
+            // 
+            // update_Password_Button
+            // 
+            this.update_Password_Button.Location = new System.Drawing.Point(361, 481);
+            this.update_Password_Button.Name = "update_Password_Button";
+            this.update_Password_Button.Size = new System.Drawing.Size(129, 63);
+            this.update_Password_Button.TabIndex = 13;
+            this.update_Password_Button.Text = "Update Password";
+            this.update_Password_Button.UseVisualStyleBackColor = true;
+            this.update_Password_Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // update_Operator_Button
+            // 
+            this.update_Operator_Button.Location = new System.Drawing.Point(496, 481);
+            this.update_Operator_Button.Name = "update_Operator_Button";
+            this.update_Operator_Button.Size = new System.Drawing.Size(129, 63);
+            this.update_Operator_Button.TabIndex = 14;
+            this.update_Operator_Button.Text = "Update Operator";
+            this.update_Operator_Button.UseVisualStyleBackColor = true;
+            this.update_Operator_Button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // save_Button
+            // 
+            this.save_Button.Location = new System.Drawing.Point(573, 412);
+            this.save_Button.Name = "save_Button";
+            this.save_Button.Size = new System.Drawing.Size(187, 40);
+            this.save_Button.TabIndex = 15;
+            this.save_Button.TabStop = false;
+            this.save_Button.Text = "Save";
+            this.save_Button.UseVisualStyleBackColor = true;
+            this.save_Button.Visible = false;
+            // 
+            // new_Operator_Button
+            // 
+            this.new_Operator_Button.Location = new System.Drawing.Point(631, 481);
+            this.new_Operator_Button.Name = "new_Operator_Button";
+            this.new_Operator_Button.Size = new System.Drawing.Size(129, 63);
+            this.new_Operator_Button.TabIndex = 16;
+            this.new_Operator_Button.Text = "New Operator";
+            this.new_Operator_Button.UseVisualStyleBackColor = true;
+            // 
+            // password_Label
+            // 
+            this.password_Label.AutoSize = true;
+            this.password_Label.Location = new System.Drawing.Point(441, 282);
+            this.password_Label.Name = "password_Label";
+            this.password_Label.Size = new System.Drawing.Size(106, 25);
+            this.password_Label.TabIndex = 17;
+            this.password_Label.Text = "Password";
+            this.password_Label.Visible = false;
+            // 
+            // password_TextBox
+            // 
+            this.password_TextBox.Location = new System.Drawing.Point(553, 279);
+            this.password_TextBox.Name = "password_TextBox";
+            this.password_TextBox.Size = new System.Drawing.Size(207, 31);
+            this.password_TextBox.TabIndex = 18;
+            this.password_TextBox.Visible = false;
+            // 
+            // confirm_Password_Label
+            // 
+            this.confirm_Password_Label.AutoSize = true;
+            this.confirm_Password_Label.Location = new System.Drawing.Point(361, 327);
+            this.confirm_Password_Label.Name = "confirm_Password_Label";
+            this.confirm_Password_Label.Size = new System.Drawing.Size(186, 25);
+            this.confirm_Password_Label.TabIndex = 19;
+            this.confirm_Password_Label.Text = "Confirm Password";
+            this.confirm_Password_Label.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(553, 324);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 31);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.Visible = false;
+            // 
+            // Operator_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 570);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.confirm_Password_Label);
+            this.Controls.Add(this.password_TextBox);
+            this.Controls.Add(this.password_Label);
+            this.Controls.Add(this.new_Operator_Button);
+            this.Controls.Add(this.save_Button);
+            this.Controls.Add(this.update_Operator_Button);
+            this.Controls.Add(this.update_Password_Button);
+            this.Controls.Add(this.language_TextBox);
+            this.Controls.Add(this.last_Name_TextBox);
+            this.Controls.Add(this.first_Name_TextBox);
+            this.Controls.Add(this.language_Label);
+            this.Controls.Add(this.last_Name_Label);
+            this.Controls.Add(this.first_Name_Label);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.operator_ID_Label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Operator_Form";
+            this.Text = "Operator_Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label operator_ID_Label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label first_Name_Label;
+        private System.Windows.Forms.Label last_Name_Label;
+        private System.Windows.Forms.Label language_Label;
+        private System.Windows.Forms.TextBox first_Name_TextBox;
+        private System.Windows.Forms.TextBox last_Name_TextBox;
+        private System.Windows.Forms.TextBox language_TextBox;
+        private System.Windows.Forms.Button update_Password_Button;
+        private System.Windows.Forms.Button update_Operator_Button;
+        private System.Windows.Forms.Button save_Button;
+        private System.Windows.Forms.Button new_Operator_Button;
+        private System.Windows.Forms.Label password_Label;
+        private System.Windows.Forms.TextBox password_TextBox;
+        private System.Windows.Forms.Label confirm_Password_Label;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
